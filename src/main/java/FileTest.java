ffa6738553e0eac6d3b2d7998f1936b6be5a5445
Iwmmeng <834638005@qq.com>
2018-06-06 12:53:22 +0800

    update pom.xml

--- src/main/java/FileTest.java
+++ src/main/java/FileTest.java
@@ -1,25 +1,36 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class FileTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File f1 = new File("/Users/huamiumiu/code/file","1.txt");
         File f2 = new File("/Users/huamiumiu/code/file","2.txt");
         File f3 = new File("/Users/huamiumiu/code/file","3.txt");
         File f4 = new File("/Users/huamiumiu/code/file");
-        try {
-            f1.createNewFile();
-            f2.createNewFile();
-            f3.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
+//        try {
+//            f1.createNewFile();
+//            f2.createNewFile();
+//            f3.createNewFile();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//        File [] ff = f4.listFiles();
+
+//        for(int i=0;i<ff.length;i++){
+//            System.out.println("file name :" + ff[i]);
+//        }
+
+//强转会发生什么？？？？
+            FileInputStream fileInputStream = new FileInputStream (f1);
+            //这个变量i在哪里体现了呢？？i的增长代表了什么？？？
+        for(int i=0;i<f1.length();i++){
+            char ch =(char)  fileInputStream.read();
+            System.out.println(ch);
         }
 
-        File [] ff = f4.listFiles();
-
-        for(int i=0;i<ff.length;i++){
-            System.out.println("file name :" + ff[i]);
-        }
 
 
     }
