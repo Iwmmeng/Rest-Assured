ab8f61b9ea2553a57ac2d165594821b2cbe657eb
Iwmmeng <834638005@qq.com>
2018-10-10 18:37:29 +0800

    新增jenkins 创建job，修改请求返回的case

--- src/main/java/restAPI/APITest1.java
+++ src/main/java/restAPI/APITest1.java
@@ -1,11 +1,19 @@
 package restAPI;
 
 import com.jayway.jsonpath.JsonPath;
+import io.restassured.builder.ResponseBuilder;
 import io.restassured.config.JsonConfig;
 import io.restassured.config.RestAssuredConfig;
+import io.restassured.filter.Filter;
+import io.restassured.filter.FilterContext;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.config.JsonPathConfig;
+import io.restassured.response.Response;
+import io.restassured.specification.FilterableRequestSpecification;
+import io.restassured.specification.FilterableResponseSpecification;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import org.apache.commons.codec.binary.Base64;
 import org.junit.Test;
 import org.openqa.selenium.json.Json;
 
@@ -90,6 +98,73 @@ public class APITest1 {
 
     }
 
+    @Test
+    public void jenkinsTest() {
+        Map loginMap = new HashMap();
+        loginMap.put("j_username", "wmmmeng");
+        loginMap.put("j_password", "wmmmeng");
+        String[] response = given().auth().basic("hogwarts", "123456")
+                .queryParams(loginMap)
+                .post("http://jenkins.testing-studio.com:8081/j_acegi_security_check")
+                .then()
+                .extract().response().getHeader("Set-Cookie").split(";");
+        System.out.println("login response" + response);
+        String[] jessidStr = response[0].split("=");
+        String jessionId = jessidStr[0];
+        String jessionIdValue = jessidStr[1];
+        System.out.println(jessionId);
+        System.out.println(jessionIdValue);
+        filters((new Filter() {
+                    @Override
+                    public Response filter(FilterableRequestSpecification requestSpec, FilterableResponseSpecification responseSpec, FilterContext ctx) {
+                        System.out.println("request filter");
+                        requestSpec.cookie(jessionId, jessionIdValue);
+                        System.out.println(requestSpec.getQueryParams());
+                        Response response = ctx.next(requestSpec, responseSpec);
+                        System.out.println("response filter");
+                        return response;
+                    }
+                })
+        );
+        given()
+                .post("http://jenkins.testing-studio.com:8081/job/wmmmengDemo/build")
+                .then()
+                .log().all()
+        ;
+    }
+    @Test
+    public void decodeBase64() {
+        filters((new Filter() {
+                    @Override
+                    public Response filter(FilterableRequestSpecification requestSpec, FilterableResponseSpecification responseSpec, FilterContext ctx) {
+                        System.out.println("request filter");
+                        System.out.println(requestSpec.getQueryParams());
 
+                        Response response = ctx.next(requestSpec, responseSpec);
+                        System.out.println("origin response");
+                        System.out.println(response.getBody().asString());
+                        System.out.println("decode response");
+                        String contentDecode = new String(Base64.decodeBase64(new String(Base64.decodeBase64(response.getBody().asString()))));
+                        System.out.println(contentDecode);
 
+                        Response newResponse = new ResponseBuilder().clone(response)
+                                .setContentType(ContentType.JSON)
+                                .setBody(contentDecode)
+                                .build();
+                        System.out.println("response filter");
+                        System.out.println(newResponse.getBody().asString());
+                        return newResponse;
+                    }
+                })
+        );
+        given().auth().basic("hogwarts", "123456")
+                .get("http://jenkins.testing-studio.com:9001/base64base64.json")
+                .then()
+                .log().all()
+                .body("data.items.quote.find{it.symbol=='SH000001'}.symbol", equalTo("SH000001"))
+                .body("data.items.quote.find{it.symbol=='SH000001'}.code", equalTo("000001"))
+                .body("data.items.quote.find{it.symbol=='SH000001'}.exchange", equalTo("SH"))
+                .body("data.items.quote.find{it.symbol=='SH000001'}.name", equalTo("上证指数"))
+        ;
+    }
 }
